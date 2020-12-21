28a41f4b0600e9b5d2a0fc5964468352704fc41a
sandeep <sandeep@knowledgelens.com>
2020-12-21 20:23:42 +0530

    First Commit

---
+++ KLService/KLService/KLService.h
@@ -0,0 +1,18 @@
+//
+//  KLService.h
+//  KLService
+//
+//  Created by sandeep kumar on 21/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KLService.
+FOUNDATION_EXPORT double KLServiceVersionNumber;
+
+//! Project version string for KLService.
+FOUNDATION_EXPORT const unsigned char KLServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KLService/PublicHeader.h>
+
+
---
+++ KLService/KLService/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  KLService
+//
+//  Created by sandeep kumar on 21/12/20.
+//
+
+import Foundation
+public class KLService {
+    private init() {
+    }
+    public static func printInitial()->String{
+       return "This is the inital Setup"
+    }
+}
